7945de87791102e584d14734687458dd73bd92a2
Ibrahima diallo <lodiajames@hotmail.com>
2022-06-23 12:44:28 -0400

    Node HTTP Example1

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
-const rectangle = require('./rectangle');
-var rect =  require('./rectangle');
+const { setServers } = require('dns');
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 3000;
 
 
-function solveRect(l,b) {
-    console.log("Solving for rectangle with l = "
-                + l + " and b = " + b);
-    rect(l,b, (err,rectangle) => {
-        if (err) {
-	        console.log("ERROR: ", err.message);
-	    }
-        else {
-            console.log("The area of the rectangle of dimensions l = "
-                + l + " and b = " + b + " is " + rectangle.area());
-            console.log("The perimeter of the rectangle of dimensions l = "
-                + l + " and b = " + b + " is " + rectangle.perimeter());
-        }
-    });
-    console.log("This statement after the call to rect()");
-};
-solveRect(2, 4);
-solveRect(3,5);
-solveRect(0,5);
-solveRect(-3,5);
+const server = http.createServer((req, res) =>{
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text-html');
+    res.end('<html><body><h1>Hello, I am Ibrahima diallo</body></html>')
+})
+
+server.listen(port, hostname, () =>{
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
